ffece1cc486959c2b6f591ba58e3bd8ef811b790
soumyasg <soumyasg@gmail.com>
2022-09-20 17:15:34 -0700

    added data annotations

--- Listings.Models/ListRecord.cs
+++ Listings.Models/ListRecord.cs
@@ -4,9 +4,11 @@ namespace Listings.Models
 {
     public class ListRecord
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         [MaxLength(10)]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
     }
 }
